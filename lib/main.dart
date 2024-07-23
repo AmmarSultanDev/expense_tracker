e0fafac244ed6553467efd861f073a85055bd66b
AmmarSultanDev <ammarsultan1445@gmail.com>
2024-07-23 13:00:03 +0100

    feat: Update NewExpense widget to use scaffoldBackgroundColor
    This commit updates the MaterialApp theme in the main.dart file to set the scaffoldBackgroundColor to a custom color. This change improves the visual appearance of the app by providing a consistent background color throughout the application.

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,10 @@ import 'package:expense_tracker/widgets/expenses.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Expenses(),
+    MaterialApp(
+      theme: ThemeData().copyWith(
+          scaffoldBackgroundColor: const Color.fromARGB(255, 220, 189, 252)),
+      home: const Expenses(),
     ),
   );
 }
